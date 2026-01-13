0c71a45cf45a08aafe5c48fbc448de8a24413a45
ceorl <dev.rocher.florian@gmail.com>
2026-01-13 06:47:33 +0100

    implementation use cases Client avec Request & Response

---
+++ src/use_cases/client_use_cases.py
@@ -0,0 +1,255 @@
+from dataclasses import dataclass
+from typing import Optional, List
+
+from src.domain.entities.entities import Client, User
+from src.domain.entities.exceptions import ValidationError
+from src.domain.entities.value_objects import Email, Telephone
+
+from src.domain.interfaces.repository import ClientRepository
+
+################################################################################################
+@dataclass
+class CreateClientRequest:
+    """Request to create a new client"""
+    fullname: str
+    email: str
+    telephone: str
+    company_name: str
+    current_user: User
+
+
+@dataclass
+class CreateClientResponse:
+    """Response to create a new client"""
+    success: bool
+    client: Optional[Client] = None
+    error: Optional[str] = None
+
+
+class CreateClientUseCase:
+    """Use case for creating a new client"""
+
+    def __init__(self, client_repository: ClientRepository):
+        self.repository = client_repository
+
+    def execute(self, request: CreateClientRequest) -> CreateClientResponse:
+
+        try:
+            if not request.current_user.is_commercial():
+                return CreateClientResponse(
+                    success=False,
+                    error="Seuls les commerciaux peuvent créer des clients"
+                )
+
+            try:
+                email = Email(request.email)
+                telephone = Telephone(request.telephone)
+            except ValidationError as e:
+                return CreateClientResponse(success=False, error=str(e))
+
+            client = Client(
+                id = None,
+                fullname=request.fullname,
+                email = email,
+                telephone = telephone,
+                company_name=request.company_name,
+                commercial_contact=request.current_user.id
+            )
+            saved_client = self.repository.save(client)
+
+            return CreateClientResponse(success=True, client=saved_client)
+
+        except Exception as e:
+            return CreateClientResponse(
+                success=False,
+                error=f"Erreur lors de la creation: {str(e)}")
+################################################################################################
+@dataclass
+class UpdateClientRequest:
+    client_id: int
+    fullname: Optional[str]
+    email: Optional[str]
+    telephone: Optional[str]
+    company_name: Optional[str]
+    current_user: User
+
+
+@dataclass
+class UpdateClientResponse:
+    success: bool
+    client: Optional[Client] = None
+    error: Optional[str] = None
+
+
+class UpdateClientUseCase:
+    """
+    Use case for updating associated client.
+    """
+    def __init__(self, client_repository: ClientRepository):
+        self.repository = client_repository
+
+    def execute(self, request: UpdateClientRequest):
+        try:
+            client = self.repository.find_by_id(request.client_id)
+            if not client:
+                return UpdateClientResponse(
+                    success=False,
+                    error=f"Client non trouvé"
+                )
+            if not request.current_user.is_commercial():
+                return UpdateClientResponse(
+                    success=False,
+                    error="Seuls les commerciaux peuvent modifier des clients"
+                )
+
+            email = telephone = fullname = company_name = None
+
+            if request.email is not None:
+                try:
+                    email = Email(request.email)
+                except ValidationError as e:
+                    return UpdateClientResponse(success=False, error=str(e))
+
+            if request.telephone is not None:
+                try:
+                    telephone = Telephone(request.telephone)
+                except ValidationError as e:
+                    return UpdateClientResponse(success=False, error=str(e))
+
+            if request.fullname is not None:
+                fullname = request.fullname
+
+            if request.company_name is not None:
+                company_name = request.company_name
+
+            client.update_info(
+                fullname = fullname,
+                email = email,
+                telephone = telephone,
+                company_name = company_name
+            )
+
+            updated_client = self.repository.save(client)
+
+            return UpdateClientResponse(success=True, client=updated_client)
+
+        except Exception as e:
+            return UpdateClientResponse(
+                success=False,
+                error=f"Erreur lors de la mise à jour: {str(e)}"
+            )
+#############################################################################
+@dataclass
+class ListClientRequest:
+    current_user: User
+
+
+@dataclass
+class ListClientResponse:
+    success: bool
+    client: List[Client] = None
+    error: Optional[str] = None
+
+
+class ListClientUseCase:
+    """Use case for listing associated clients"""
+    def __init__(self, client_repository: ClientRepository):
+        self.repository = client_repository
+
+    def execute(self, request: ListClientRequest) -> ListClientResponse:
+        try:
+            # TODO Acces en lecture seule pour tout le monde
+            if not request.current_user.is_commercial():
+                return ListClientResponse(
+                    success=False,
+                    error="Seuls les commerciaux peuvent récupérer des clients"
+                )
+            all_client = self.repository.find_all()
+            return ListClientResponse(success=True, client=all_client)
+
+        except Exception as e:
+            return ListClientResponse(success=False, error=str(e))
+#############################################################################
+@dataclass
+class GetClientRequest:
+    client_id: int
+    current_user: User
+
+
+@dataclass
+class GetClientResponse:
+    success: bool
+    client: Optional[Client] = None
+    error: Optional[str] = None
+
+
+class GetClientUseCase:
+    def __init__(self, repository: ClientRepository):
+        self.repository = repository
+
+    def execute(self, request: GetClientRequest):
+        try:
+            # TODO Acces en lecture seule pour tout le monde
+            if not request.current_user.is_commercial():
+                return GetClientResponse(
+                    success=False,
+                    error="Seuls les commerciaux peuvent récupérer des clients"
+                )
+
+            client = self.repository.find_by_id(request.client_id)
+
+            if not client:
+                return GetClientResponse(
+                    success=False,
+                    error=f"Client non trouvé"
+                )
+
+            return GetClientResponse(success=True, client=client)
+
+        except Exception as e:
+            return GetClientResponse(
+                success=False,
+                error=f"Erreur lors de la recherche: {str(e)}"
+            )
+
+##############################################################################
+@dataclass
+class DeleteClientRequest:
+    client_id: int
+    current_user: User
+
+
+@dataclass
+class DeleteClientResponse:
+    success: bool
+    error: Optional[str] = None
+
+
+class DeleteClientUseCase:
+    def __init__(self, repository: ClientRepository):
+        self.repository = repository
+
+    def execute(self, request: DeleteClientRequest):
+
+        try:
+            client = self.repository.find_by_id(request.client_id)
+            if not client:
+                return DeleteClientResponse(
+                    success=False,
+                    error=f"Client non trouvé"
+                )
+
+            if not request.current_user.is_commercial():
+                return DeleteClientResponse(
+                    success=False,
+                    error="Seuls les commerciaux peuvent supprimer des clients"
+                )
+
+            self.repository.delete(client.id)
+            return DeleteClientResponse(success=True)
+
+        except Exception as e:
+            return DeleteClientResponse(
+                success=False,
+                error=f"Erreur lors de la recherche: {str(e)}"
+            )
