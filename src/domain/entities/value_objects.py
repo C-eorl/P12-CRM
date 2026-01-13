f694a34fbb28df62e190265f0474ba4a66d232cf
ceorl <dev.rocher.florian@gmail.com>
2026-01-13 06:45:24 +0100

    implementation Object value métier Telephone, Email

---
+++ src/domain/entities/value_objects.py
@@ -0,0 +1,44 @@
+import re
+from dataclasses import dataclass
+
+from src.domain.entities.exceptions import InvalidPhoneError, InvalidEmailError
+
+
+@dataclass(frozen=True)
+class Telephone:
+    """Object representing a telephone number"""
+    number: str
+
+    def __post_init__(self):
+        if not self.is_valid(self.number):
+            raise InvalidPhoneError(f"Télephone invalide: {self.number}")
+
+    def __str__(self) -> str:
+        return self.number
+
+    def is_valid(telephone: str)-> bool:
+        """Validate telephone format"""
+        if not telephone:
+            return False
+        pattern = r'^\+?[\d\s\-\(\)]{8,20}$'
+        return bool(re.match(pattern, telephone))
+
+
+@dataclass(frozen=True)
+class Email:
+    """Object representing an email address"""
+    address: str
+
+    def __post_init__(self):
+        if not self.is_valid(self.address):
+            raise InvalidEmailError(f"E-mail invalide: {self.address}")
+
+    def __str__(self) -> str:
+        return self.address
+
+    def is_valid(email: str) -> bool:
+        """Validate email format"""
+        if not email:
+            return False
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return bool(re.match(pattern, email))
