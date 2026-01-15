412366898c66c746bf6afdf118bdec1a210ddabf
ceorl <dev.rocher.florian@gmail.com>
2026-01-15 23:13:55 +0100

    add correct type + add method Contrat / Event

--- src/domain/entities/entities.py
+++ src/domain/entities/entities.py
@@ -2,13 +2,14 @@ from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Optional
 
-from src.domain.entities.enums import Role
-from src.domain.entities.value_objects import Email, Telephone
+from src.domain.entities.enums import Role, ContractStatus
+from src.domain.entities.exceptions import BusinessRuleViolation
+from src.domain.entities.value_objects import Email, Telephone, Money
 
 
 @dataclass
 class User:
-    id: Optional[int]
+    id: int
     full_name: str
     email: Email
     password: str
@@ -23,15 +24,17 @@ class User:
     def is_gestion(self) -> bool:
         return self.role is Role.GESTION
 
+    # TODO ajout d'un UserPolicy pour les permission si besoin
+
 
 @dataclass
 class Client:
-    id: Optional[int]
+    id: int
     fullname: str
     email: Email
     telephone: Telephone
     company_name: str
-    commercial_contact: User
+    commercial_contact: int
 
     created_at: datetime = field(default_factory=datetime.now)
     updated_at: datetime = field(default_factory=datetime.now)
@@ -48,29 +51,118 @@ class Client:
             self.company_name = company_name
         self.updated_at = datetime.now()
 
-    def can_be_updated(self, user: User) -> bool:
-        if user.id == self.commercial_contact.id:
+    def can_be_updated_by(self, user: User) -> bool:
+        if user.id == self.commercial_contact:
             return True
         return False
 
 @dataclass
 class Contrat:
     id: int
-    client: Client
-    commercial_contact: User
-    contrat_amount: int
-    balance_due: int
-    signed: bool
+    client: int
+    commercial_contact: int
+    contrat_amount: Money
+    balance_due: Money
+    status: ContractStatus
 
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+
+    def sign(self):
+        """Sign the contrat"""
+        if self.status == ContractStatus.SIGNED:
+            raise BusinessRuleViolation("Contrat déjà signé")
+        self.status = ContractStatus.SIGNED
+
+    def record_payment(self, payment: Money):
+        if payment > self.balance_due:
+            raise BusinessRuleViolation(
+                "Le montant est plus grand que le reste à payer"
+            )
+
+        self.balance_due = self.balance_due - payment
+
+    def is_fully_paid(self) -> bool:
+        return self.balance_due.amount == 0
+
+    def can_be_updated_by(self, user: User) -> bool:
+        """Verified if user can be updated"""
+        if user.id == self.commercial_contact:
+            return True
+        if user.is_gestion():
+            return True
+        return False
+
+    def __str__(self):
+        return f"Contrat #{self.id} - {self.status} ({self.contrat_amount})"
 
 @dataclass
 class Event:
-    name: str
     id: int
+    name: str
     contrat_id: int
-    client_name: str
-    contact_client: Client
-    support_contact: User
+    client_id: int
+    start_date: datetime
+    end_date: datetime
+    support_contact_id: Optional[User]
     location: str
     attendees: int
     notes: str
+
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+
+    def __post_init__(self):
+        """Validation"""
+        if self.start_date < datetime.now():
+            raise BusinessRuleViolation(
+                "La date de début est déjà passé"
+            )
+        if self.end_date <= self.start_date:
+            raise BusinessRuleViolation(
+                "La date de fin doit être après la date de début"
+            )
+        if self.attendees <= 0:
+            raise BusinessRuleViolation(
+                "Le nombre de participants doit être positif"
+            )
+
+    def assign_support(self, user: User):
+        """Assigned support user for this event"""
+        if not user.is_support():
+            raise PermissionError(
+                "Seul les membre du support peut être assigné à un évènement"
+            )
+        self.support_contact_id = user
+
+    def has_support_contact(self) -> bool:
+        """Verified if assigned support user"""
+        return self.support_contact_id is not None
+
+    def can_be_updated_by(self, user: User) -> bool:
+        """Verified if user can be updated"""
+        if user.is_gestion():
+            return True
+        if user.is_support() and user.id == self.support_contact_id:
+            return True
+        return False
+
+    def update_info(self, name: Optional[str], location: Optional[str],
+                    attendees: Optional[int], notes: Optional[str]):
+        """Updates info about the event"""
+        if name:
+            self.name = name
+        if location:
+            self.location = location
+        if attendees:
+            if attendees < 0:
+                raise BusinessRuleViolation(
+                    "Le nombre de participants doit être positif"
+                )
+            self.attendees = attendees
+        if notes:
+            self.notes = notes
+
+    def __str__(self):
+        support = f"Support: {self.support_contact_id}" if self.support_contact_id else "Sans Support"
+        return f"Event: {self.name} - {self.start_date.strftime('%d/%m/%Y')} ({support})"
