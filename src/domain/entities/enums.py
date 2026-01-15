0517999ebad58309dcf9dde962189bba6e908ede
ceorl <dev.rocher.florian@gmail.com>
2026-01-15 23:12:55 +0100

    add contratstatus + refacto not @dataclass

--- src/domain/entities/enums.py
+++ src/domain/entities/enums.py
@@ -2,8 +2,12 @@ from dataclasses import dataclass
 from enum import Enum
 
 
-@dataclass(frozen=True)
 class Role(Enum):
     COMMERCIAL = "COMMERCIAL"
     SUPPORT = "SUPPORT"
     GESTION = "GESTION"
+
+class ContractStatus(Enum):
+    SIGNED = "SIGNED"
+    UNSIGNED = "UNSIGNED"
+    CANCELED = "CANCELED"
