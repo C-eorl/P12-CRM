1dac40433085ac7bff620247eb11e088c821e27a
ceorl <dev.rocher.florian@gmail.com>
2026-01-16 15:05:56 +0100

    refacto suppression fixture

--- test/units/domain/entities/test_contrat_entity.py
+++ test/units/domain/entities/test_contrat_entity.py
@@ -2,39 +2,13 @@ from decimal import Decimal
 
 import pytest
 
-from src.domain.entities.entities import Contrat, Client, User
-from src.domain.entities.enums import ContractStatus, Role
+from entyties_fixtures import user_commercial, contrat
+from src.domain.entities.entities import Contrat
+
+from src.domain.entities.enums import ContractStatus
 from src.domain.entities.exceptions import BusinessRuleViolation
-from src.domain.entities.value_objects import Email, Telephone, Money
+from src.domain.entities.value_objects import Money
 
-
-@pytest.fixture
-def user():
-    return User(id=1, full_name="test test",
-                email=Email("test@test.com"), password="sfsefs",
-                role=Role.COMMERCIAL)
-
-@pytest.fixture
-def client():
-    return Client(
-        id=5,
-        fullname="test test",
-        email=Email("mail@test.com"),
-        telephone=Telephone("0245785689"),
-        company_name="test",
-        commercial_contact= 1
-    )
-
-@pytest.fixture(scope="function")
-def contrat():
-    return Contrat(
-        id=1,
-        client= 5,
-        commercial_contact= 1,
-        contrat_amount=Money(100),
-        balance_due=Money(100),
-        status= ContractStatus.UNSIGNED
-    )
 
 
 def test_contrat_entity(contrat):
@@ -67,5 +41,5 @@ def test_contrat_is_not_fully_paid(contrat):
     contrat.record_payment(Money(10))
     assert contrat.is_fully_paid() is False
 
-def test_can_be_updated_by(contrat, user):
-    assert contrat.can_be_updated_by(user) is True
+def test_can_be_updated_by(contrat, user_commercial):
+    assert contrat.can_be_updated_by(user_commercial) is True
