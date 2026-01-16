f9a87b697c2c5a85d5256d66e4b880fb823d66c4
ceorl <dev.rocher.florian@gmail.com>
2026-01-16 15:03:54 +0100

    refacto. fixture separée (entyties_fixture.py) pour qu'elle soit général aux tests

--- test/units/domain/entities/test_user_entity.py
+++ test/units/domain/entities/test_user_entity.py
@@ -2,41 +2,31 @@ from src.domain.entities.entities import User
 from src.domain.entities.enums import Role
 from src.domain.entities.value_objects import Email
 
-def test_user_create():
-    user = User(id=1, full_name="test test",
-                email=Email("test@test.com"), password="sfsefs",
-                role=Role.COMMERCIAL)
+from entyties_fixtures import user_commercial, user_support, user_gestion
 
-    assert type(user.id) == int
-    assert type(user.full_name) == str
-    assert type(user.email) == Email
-    assert type(user.password) == str
-    assert type(user.role) == Role
+def test_user_create(user_commercial):
 
-def test_user_is_commercial():
-    user = User(id=1, full_name="test test",
-         email=Email("test@test.com"), password="sfsefs",
-         role=Role.COMMERCIAL)
+    assert type(user_commercial.id) == int
+    assert type(user_commercial.full_name) == str
+    assert type(user_commercial.email) == Email
+    assert type(user_commercial.password) == str
+    assert type(user_commercial.role) == Role
 
-    assert user.is_commercial() is True
-    assert user.is_support() is False
-    assert user.is_gestion() is False
+def test_user_is_commercial(user_commercial):
 
-def test_user_is_support():
-    user = User(id=1, full_name="test test",
-         email=Email("test@test.com"), password="sfsefs",
-         role=Role.SUPPORT)
+    assert user_commercial.is_commercial() is True
+    assert user_commercial.is_support() is False
+    assert user_commercial.is_gestion() is False
 
-    assert user.is_commercial() is False
-    assert user.is_support() is True
-    assert user.is_gestion() is False
+def test_user_is_support(user_support):
 
-def test_user_is_gestion():
-    user = User(id=1, full_name="test test",
-         email=Email("test@test.com"), password="sfsefs",
-         role=Role.GESTION)
+    assert user_support.is_commercial() is False
+    assert user_support.is_support() is True
+    assert user_support.is_gestion() is False
 
-    assert user.is_commercial() is False
-    assert user.is_support() is False
-    assert user.is_gestion() is True
+def test_user_is_gestion(user_gestion):
+
+    assert user_gestion.is_commercial() is False
+    assert user_gestion.is_support() is False
+    assert user_gestion.is_gestion() is True
 
