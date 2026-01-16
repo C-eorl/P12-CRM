3276d84d726a3ced3120bb7b93d67c210dd256ad
ceorl <dev.rocher.florian@gmail.com>
2026-01-16 15:04:12 +0100

    file fixture global

---
+++ test/units/domain/entities/entyties_fixtures.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+import pytest
+
+from src.domain.entities.entities import User, Client, Contrat, Event
+from src.domain.entities.enums import Role, ContractStatus
+from src.domain.entities.value_objects import Email, Telephone, Money
+
+
+@pytest.fixture
+def user_commercial():
+    return User(id=1, full_name="test test",
+                email=Email("test@test.com"), password="sfsefs",
+                role=Role.COMMERCIAL)
+
+@pytest.fixture
+def user_support():
+    return User(id=2, full_name="test test",
+                email=Email("test@test.com"), password="sfsefs",
+                role=Role.SUPPORT)
+
+@pytest.fixture
+def user_gestion():
+    return User(id=3, full_name="test test",
+                email=Email("test@test.com"), password="sfsefs",
+                role=Role.GESTION)
+
+@pytest.fixture
+def user_commercial2():
+    return User(id=2, full_name="test2", email=Email("test2@test.fr"),
+                password="dfsdfsf", role=Role.COMMERCIAL,)
+
+@pytest.fixture
+def user_support2():
+    return User(id=45, full_name="test test",
+                email=Email("test@test.com"), password="sfsefs",
+                role=Role.SUPPORT)
+
+@pytest.fixture
+def client(user_commercial):
+    return Client(
+        id=1,fullname="test test", email=Email('test@test.fr'),
+        telephone=Telephone('0645789845'), company_name="company_test",
+        commercial_contact=user_commercial.id
+    )
+
+@pytest.fixture(scope="function")
+def contrat():
+    return Contrat(
+        id=1,
+        client= 5,
+        commercial_contact= 1,
+        contrat_amount=Money(100),
+        balance_due=Money(100),
+        status= ContractStatus.UNSIGNED
+    )
+
+@pytest.fixture
+def event():
+    return Event(
+        id=1,
+        name="test event",
+        contrat_id= 1,
+        client_id= 1,
+        support_contact_id= 2,
+        start_date= datetime(2026, 5,15),
+        end_date= datetime(2026, 5,20),
+        location="2 rue des test, Nantes",
+        attendees= 150,
+        notes=""
+    )
