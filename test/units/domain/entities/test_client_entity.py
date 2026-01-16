995b5aacab9c437d91bd6cbeb2570c5581e8aa69
ceorl <dev.rocher.florian@gmail.com>
2026-01-16 15:05:42 +0100

    refacto suppression fixture

--- test/units/domain/entities/test_client_entity.py
+++ test/units/domain/entities/test_client_entity.py
@@ -1,24 +1,10 @@
 from datetime import datetime
 
-import pytest
 
-from src.domain.entities.entities import Client, User
-from src.domain.entities.enums import Role
+
+from entyties_fixtures import user_commercial, client, user_commercial2
 from src.domain.entities.value_objects import Email, Telephone
 
-@pytest.fixture
-def user_commercial():
-    return User(id=1, full_name="test test",
-                email=Email("test@test.com"), password="sfsefs",
-                role=Role.COMMERCIAL)
-
-@pytest.fixture
-def client(user_commercial):
-    return Client(
-        id=1,fullname="test test", email=Email('test@test.fr'),
-        telephone=Telephone('0645789845'), company_name="company_test",
-        commercial_contact=user_commercial.id
-    )
 
 def test_client_create(client):
 
@@ -44,9 +30,7 @@ def test_client_update_info(client):
     assert client.company_name == company_name
     assert client.updated_at != client.created_at
 
-def test_client_can_by_update(client, user_commercial):
-    user = User(id=2, full_name="test2", email=Email("test2@test.fr"),
-                password="dfsdfsf", role=Role.COMMERCIAL,)
+def test_client_can_by_update(client, user_commercial, user_commercial2):
 
-    assert client.can_be_updated_by(user) is False
+    assert client.can_be_updated_by(user_commercial2) is False
     assert client.can_be_updated_by(user_commercial) is True
