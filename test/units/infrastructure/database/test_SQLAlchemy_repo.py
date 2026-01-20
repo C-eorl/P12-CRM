78b9149690d51289d76cfed07e94c7d7563044b4
ceorl <dev.rocher.florian@gmail.com>
2026-01-20 15:59:14 +0100

    implementation (suite) test repo SQLAlchemy

--- test/units/infrastructure/database/test_SQLAlchemy_repo.py
+++ test/units/infrastructure/database/test_SQLAlchemy_repo.py
@@ -1,4 +1,11 @@
+from typing import List
+
+from src.domain.entities.entities import Client
+from src.infrastructures.database.models import ClientModel
+
+
 def test_save_client_create(client_SQLAlchemy_repository, client):
+    """test save method for create client"""
     saved_client = client_SQLAlchemy_repository.save(client)
 
     assert saved_client.id is not None
@@ -7,10 +14,41 @@ def test_save_client_create(client_SQLAlchemy_repository, client):
 
 
 def test_save_client_update(client_SQLAlchemy_repository, client):
-    saved_client = client_SQLAlchemy_repository.save(client)
+    """test save method for update client"""
+    saved_client = client_SQLAlchemy_repository.find_by_id(4)
 
     saved_client.fullname = "SQL test"
     updated_client = client_SQLAlchemy_repository.save(saved_client)
 
     assert updated_client.fullname == "SQL test"
     assert updated_client.id == saved_client.id
+
+def test_find_by_id(client_SQLAlchemy_repository):
+    """test find by id method """
+    find_client = client_SQLAlchemy_repository.find_by_id(4)
+
+    assert isinstance(find_client, Client)
+    assert find_client.email.address == "test@test.fr"
+
+def test_find_by_invalid_id(client_SQLAlchemy_repository):
+    """test find all method """
+    find_client = client_SQLAlchemy_repository.find_by_id(45)
+
+    assert find_client is None
+
+def test_find_all(client_SQLAlchemy_repository, session):
+    """test find all method """
+    all_clients = client_SQLAlchemy_repository.find_all()
+
+    assert isinstance(all_clients, List)
+    actual_count_client = session.query(ClientModel).count()
+    assert len(all_clients) == actual_count_client
+
+def test_delete(client_SQLAlchemy_repository, session):
+    """test delete method """
+    # vérifier si id existe sinon créer un client
+    delete_client = client_SQLAlchemy_repository.delete(5)
+
+    assert delete_client is None
+    actual_count_client = session.query(ClientModel).count()
+    assert actual_count_client == 3
