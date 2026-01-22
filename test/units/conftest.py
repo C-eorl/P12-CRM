9d31a501532438a8810d455f8425aa38ee1a08e2
ceorl <dev.rocher.florian@gmail.com>
2026-01-22 10:17:19 +0100

    refacto change env. variable

--- test/units/conftest.py
+++ test/units/conftest.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL_TEST")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 @pytest.fixture(scope="module")
 def user_commercial():
