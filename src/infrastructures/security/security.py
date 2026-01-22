39c7df45916ae99b5bd5e983dea841df7dbfc4fc
ceorl <dev.rocher.florian@gmail.com>
2026-01-22 10:19:21 +0100

    implementation hash password, manage jwt token and token storage

---
+++ src/infrastructures/security/security.py
@@ -0,0 +1,82 @@
+import os
+from pathlib import Path
+from datetime import datetime, timedelta
+from typing import Optional
+
+from dotenv import load_dotenv
+import bcrypt
+import jwt
+
+from src.domain.entities.entities import User
+
+load_dotenv()
+
+class BcryptPasswordHasher:
+
+    def hash_password(self, password: str) -> str:
+        """Hash a password """
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed.decode('utf8')
+
+    def verify_password(self, password: str, hashed: str) -> bool:
+        """Verify a hashed password """
+        if bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8')):
+            return True
+        return False
+
+
+class JWTTokenManager:
+    def __init__(self):
+        self.expiration = int(os.getenv("JWT_EXPIRATION"))
+        self.algorithm = "HS256"
+        self.secret_key = os.getenv("JWT_SECRET_KEY")
+
+    def create_token(self, user_id: int) -> str:
+        """Create a JWT token for user"""
+        payload = {
+            'user_id': user_id,
+            'exp': datetime.now() + timedelta(hours=self.expiration),
+            'iat': datetime.now(),
+        }
+        token = jwt.encode(payload, self.secret_key, algorithm=self.algorithm)
+
+        return token
+
+    def decode_token(self, token: str) -> Optional[int]:
+        """Decode JWT token"""
+        try:
+            payload = jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
+            return payload['user_id']
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+
+
+class TokenStore:
+
+    TOKEN_DIR = Path(__file__).resolve().parent
+    TOKEN_FILE = TOKEN_DIR / "token"
+
+    @classmethod
+    def save_token(cls, token: str):
+        """Enregistre le token"""
+        cls.TOKEN_FILE.write_text(token)
+
+    @classmethod
+    def get_token(cls) -> Optional[str]:
+        """Récupère le token"""
+        if not cls.TOKEN_FILE.exists():
+            return None
+        return cls.TOKEN_FILE.read_text().strip()
+
+    @classmethod
+    def delete_token(cls):
+        """Supprime le token"""
+        if cls.TOKEN_FILE.exists():
+            cls.TOKEN_FILE.unlink()
+    @classmethod
+    def has_token(cls) -> bool:
+        """Vérifie si un token est enregistré"""
+        return cls.TOKEN_FILE.exists()
