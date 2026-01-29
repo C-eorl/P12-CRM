1c186f82a9605828ccd53526059f4a2dbaf28fd4
ceorl <dev.rocher.florian@gmail.com>
2026-01-29 15:45:06 +0100

    add commands auth

--- src/presentation/cli/commands/auth_commands.py
+++ src/presentation/cli/commands/auth_commands.py
@@ -1,18 +1,19 @@
 import typer
 
-from src.infrastructures.database.session import get_session
-from src.infrastructures.repositories.SQLAchemy_repository import SQLAchemyUserRepository
+from src.infrastructures.repositories.SQLAchemy_repository import SQLAlchemyUserRepository
 from src.infrastructures.security.security import BcryptPasswordHasher, JWTTokenManager
 from src.use_cases.auth_use_cases import AuthenticateUseCase, AuthenticateRequest
 
 auth_app = typer.Typer()
 
 @auth_app.command()
-def login(email: str, password: str):
+def login(ctx: typer.Context,
+        email: str = typer.Option(prompt=True),
+        password: str = typer.Option(prompt=True, hide_input=True)
+):
     """Connect user to email & password"""
     # execute use case authenticate
-    session = get_session()
-    repo = SQLAchemyUserRepository(session)
+    repo = SQLAlchemyUserRepository(ctx.obj["session"])
     password_hasher = BcryptPasswordHasher()
     token_manager = JWTTokenManager()
     use_case = AuthenticateUseCase(repo, password_hasher, token_manager)
