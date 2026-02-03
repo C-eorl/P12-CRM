380274ef78662fe5f1a7196122c1e7900fb7a52d
ceorl <dev.rocher.florian@gmail.com>
2026-02-03 06:57:13 +0100

    - add file centralize function helper

---
+++ helpers/helpers.py
@@ -0,0 +1,28 @@
+from src.infrastructures.database.session import get_session
+from src.infrastructures.repositories.SQLAchemy_repository import SQLAlchemyUserRepository
+from src.infrastructures.security.security import TokenStore, JWTTokenManager
+
+
+def normalize(value: str) -> str | None:
+    return value if value not in ('', 0) else None
+
+
+def get_current_user() :
+
+    if not TokenStore.has_token():
+        print("token non trouvé")
+        return None
+    if TokenStore.has_expired():
+        print("token expiré")
+        return None
+
+    token = TokenStore.get_token()
+    jwt = JWTTokenManager()
+    payload = jwt.decode_token(token)
+    if payload is None:
+        TokenStore.delete_token()
+        print("token non conforme")
+        return None
+    repo = SQLAlchemyUserRepository(get_session())
+    user = repo.find_by_id(payload["user_id"])
+    return {"user_current_id": user.id, "user_current_role": user.role}
