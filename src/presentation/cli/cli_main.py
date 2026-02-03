45fb0d6136c519c7784f4319496347fd91c5ed86
ceorl <dev.rocher.florian@gmail.com>
2026-02-03 06:59:21 +0100

    - refacto & clean

--- src/presentation/cli/cli_main.py
+++ src/presentation/cli/cli_main.py
@@ -1,8 +1,9 @@
 import typer
+from rich.console import Console
 
+from helpers.helpers import get_current_user
 from src.infrastructures.database.session import get_session
-from src.infrastructures.repositories.SQLAchemy_repository import SQLAlchemyUserRepository
-from src.infrastructures.security.security import TokenStore, JWTTokenManager
+from src.infrastructures.security.security import TokenStore
 from src.presentation.cli.commands.auth_commands import auth_app
 from src.presentation.cli.commands.client_commands import client_app
 from src.presentation.cli.commands.contrat_commands import contrat_app
@@ -10,6 +11,7 @@ from src.presentation.cli.commands.event_commands import event_app
 from src.presentation.cli.commands.user_commands import user_app
 
 app = typer.Typer()
+console = Console()
 
 app.add_typer(auth_app, name="auth", help="Authentification")
 app.add_typer(user_app, name="user", help="Commandes liées aux utilisateurs")
@@ -18,33 +20,26 @@ app.add_typer(contrat_app, name="contrat", help="Commandes liées aux contrats")
 app.add_typer(event_app, name="event", help="Commandes liées aux évènements")
 
 
-def get_current_user() :
-    token = TokenStore.get_token()
-    if token is None:
-        return None
 
-    jwt = JWTTokenManager()
-    payload = jwt.decode_token(token)
-    if payload is None:
-        return None
-    repo = SQLAlchemyUserRepository(get_session())
-    user = repo.find_by_id(payload["user_id"])
-    return {"user_id": user.id, "user_role": user.role}
 
 @app.callback()
 def main(ctx: typer.Context):
+    """
+    Callback auth verification before command
+    Initialisation Context and add session(DB), current_user(dict)
+    """
+
+
     ctx.ensure_object(dict)
 
     ctx.obj["session"] = get_session()
     ctx.obj["current_user"] = get_current_user()
 
-    token = TokenStore.has_token()
-    if ctx.invoked_subcommand != "auth":
-        if not token:
-            typer.echo("Veuillez vous connecter via - auth login -")
+    if ctx.obj["current_user"] is None:
+        if ctx.invoked_subcommand == "auth":
+            pass
+        if ctx.invoked_subcommand != "auth":
+            console.print("[red]Veuillez vous connecter via - auth login -[/red]")
             raise typer.Exit(1)
 
-    if ctx.invoked_subcommand == "auth":
-        if token:
-            typer.echo("Vous êtes déjà connecté")
-            raise typer.Exit(1)
+
