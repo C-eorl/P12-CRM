800f7088efbd2337f7da6a08bd4fca55dd01d456
ceorl <dev.rocher.florian@gmail.com>
2026-02-03 06:58:53 +0100

    - refacto UserPolicy to ABAC system

--- src/domain/policies/user_policy.py
+++ src/domain/policies/user_policy.py
@@ -1,45 +1,66 @@
-from src.domain.entities.entities import User
+import json
+import os
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class RequestPolicy:
+    user: dict
+    ressource: str
+    action: str
+    context: Any | None = None
 
 
 class UserPolicy:
-    def __init__(self, user: User):
-        self.user = user
+    """
+    Policy object for permission user
+    Request : RequestPolicy object
+    """
+    def __init__(self, request: RequestPolicy):
 
-    def can_create_client(self) -> bool:
-        return self.user.is_commercial()
+        self.permission = self.get_permission()
+        self.request = request
 
-    def can_update_client(self) -> bool:
-        return self.user.is_commercial()
+    def get_permission(self) -> dict:
+        """ Get json file permission """
+        dir_path = os.path.dirname(__file__)
+        path = os.path.join(dir_path, 'permission.json')
 
-    def can_delete_client(self) -> bool:
-        return self.user.is_admin()
+        try:
+            with open(path) as json_file:
+                permission = json.load(json_file)
+                return permission
+        except FileNotFoundError:
+            raise FileNotFoundError('Fichier de permission non trouvé')
 
-    def can_create_contrat(self) -> bool:
-        return self.user.is_gestion()
+    def is_allowed(self) -> bool:
+        """
+        Check if user is allowed
+        rule => "role", "action", "context"
+        :return true | false
+        """
+        rule = (
+            self.permission
+            .get(self.request.user["user_current_role"].value, {})
+            .get(self.request.ressource, {})
+            .get(self.request.action, None)
+        )
 
-    def can_update_contrat(self) -> bool:
-        return self.user.is_gestion() or self.user.is_commercial()
+        match rule:
+            case None : return False
+            case "*" : return True
+            case dict() if len(rule) == 0 : return True
+            case _ : return self.evaluation_context(rule)
 
-    def can_delete_contrat(self) -> bool:
-        return self.user.is_admin()
 
-    def can_create_event(self) -> bool:
-        return self.user.is_commercial()
-
-    def can_update_event(self) -> bool:
-        return self.user.is_support() or self.user.is_gestion()
-
-    def can_assign_support(self) -> bool:
-        return self.user.is_gestion()
-
-    def can_delete_event(self) -> bool:
-        return self.user.is_admin()
-
-    def can_create_user(self) -> bool:
-        return self.user.is_gestion()
-
-    def can_update_user(self) -> bool:
-        return self.user.is_gestion()
-
-    def can_delete_user(self) -> bool:
-        return self.user.is_gestion()
+    def evaluation_context(self, rule):
+        """ Evaluation rule context in json permission"""
+        for key, value in rule.items():
+            condition = getattr(self.request.context, key)
+            expected = self.request.user.get(value)
+            if expected != condition:
+                print("evaluation context invalide")
+                return False
+        print("evaluation context")
+        return True
