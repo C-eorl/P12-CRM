3471f6a175e8697e5728d8d82347e842e965f58e
ceorl <dev.rocher.florian@gmail.com>
2026-02-03 07:01:00 +0100

    - implementation callback for access - add command logout

--- src/presentation/cli/commands/auth_commands.py
+++ src/presentation/cli/commands/auth_commands.py
@@ -1,10 +1,25 @@
 import typer
+from rich.console import Console
 
 from src.infrastructures.repositories.SQLAchemy_repository import SQLAlchemyUserRepository
-from src.infrastructures.security.security import BcryptPasswordHasher, JWTTokenManager
+from src.infrastructures.security.security import BcryptPasswordHasher, JWTTokenManager, TokenStore
 from src.use_cases.auth_use_cases import AuthenticateUseCase, AuthenticateRequest
 
 auth_app = typer.Typer()
+console = Console()
+
+@auth_app.callback()
+def access(ctx: typer.Context):
+    """Callback - for login & logout commands, verify token presence """
+    token = TokenStore.has_token()
+    if ctx.invoked_subcommand == "login":
+        if token:
+            console.print("[green]Vous êtes déjà connecté[/green]")
+            raise typer.Exit(1)
+    if ctx.invoked_subcommand == "logout":
+        if not token:
+            console.print("[red]Vous n'êtes pas connecté[/red]")
+            raise typer.Exit(1)
 
 @auth_app.command()
 def login(ctx: typer.Context,
@@ -12,7 +27,6 @@ def login(ctx: typer.Context,
         password: str = typer.Option(prompt=True, hide_input=True)
 ):
     """Connect user to email & password"""
-    # execute use case authenticate
     repo = SQLAlchemyUserRepository(ctx.obj["session"])
     password_hasher = BcryptPasswordHasher()
     token_manager = JWTTokenManager()
@@ -21,7 +35,15 @@ def login(ctx: typer.Context,
     request = AuthenticateRequest(email, password)
     response = use_case.execute(request)
     if response.success is False:
-        typer.echo(f"{response.error}")
+        console.print(f"{response.error}")
     else:
-        typer.echo("Vous êtes connecté")
+        console.print("[green]Vous êtes connecté[/green]")
+
+@auth_app.command()
+def logout():
+    """Logout user"""
+    token_storage = TokenStore()
+    if token_storage.has_token():
+        token_storage.delete_token()
+        console.print("[green]Vous êtes déconnecté[/green]")
 
