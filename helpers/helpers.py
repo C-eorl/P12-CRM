ca5ecb30a007516b1db67e78306e0fc0aaac469c
ceorl <dev.rocher.florian@gmail.com>
2026-02-05 16:40:19 +0100

    add delete token when expired

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -14,6 +14,7 @@ def get_current_user() :
         return None
     if TokenStore.has_expired():
         print("token expiré")
+        TokenStore.delete_token()
         return None
 
     token = TokenStore.get_token()
