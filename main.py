8a436c0d7736180994e4449b43a5d8ae8466b1e1
ceorl <dev.rocher.florian@gmail.com>
2026-02-12 15:08:30 +0100

    - add sentry init()

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import dotenv
+dotenv.load_dotenv()
+
+from src.infrastructures.sentry.sentry import init_sentry
+
 from src.presentation.cli.cli_main import app
 
 
 def main():
+
+    init_sentry()
     app()
 
 if __name__ == '__main__':
