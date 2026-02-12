75aeb6ebf64b2c66447c4d37f853c2f0c9322dbf
ceorl <dev.rocher.florian@gmail.com>
2026-02-12 15:13:57 +0100

    - add set_user for sentry - add call_on_close => session

--- src/presentation/cli/cli_main.py
+++ src/presentation/cli/cli_main.py
@@ -1,9 +1,10 @@
 import typer
 from rich.console import Console
+from sentry_sdk import set_user
 
+from helpers.helper_cli import error_display
 from helpers.helpers import get_current_user
-from src.infrastructures.database.session import get_session
-from src.infrastructures.security.security import TokenStore
+from src.infrastructures.database.session import get_session, init_db
 from src.presentation.cli.commands.auth_commands import auth_app
 from src.presentation.cli.commands.client_commands import client_app
 from src.presentation.cli.commands.contrat_commands import contrat_app
@@ -33,11 +34,27 @@ def main(ctx: typer.Context):
     ctx.obj["session"] = get_session()
     ctx.obj["current_user"] = get_current_user()
 
+
     if ctx.obj["current_user"] is None:
         if ctx.invoked_subcommand == "auth":
             pass
         if ctx.invoked_subcommand != "auth":
-            console.print("[red]Veuillez vous connecter via - auth login -[/red]")
+            error_display("Erreur Authentification", "Veuillez vous connecter via - auth login -")
             raise typer.Exit(1)
 
+    if ctx.obj["current_user"] is not None:
+        set_user({
+            "id": ctx.obj["current_user"]["user_current_id"],
+            "role": ctx.obj["current_user"]["user_current_role"].value
+        })
+    ctx.call_on_close(ctx.obj["session"].close)
 
+@app.command()
+def init():
+    """Command initialize database"""
+    try:
+        console.print("[yellow]Initialisation de la base de données...[/yellow]")
+        init_db()
+        console.print("[green]* Base de données initialisée avec succès![/green]")
+    except Exception as e:
+        console.print(f"[red]✗ Erreur lors de l'initialisation: {str(e)}[/red]")
