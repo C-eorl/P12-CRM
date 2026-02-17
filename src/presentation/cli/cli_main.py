6bb930ae368f8081762b271b3dca979b5e2f694e
ceorl <dev.rocher.florian@gmail.com>
2026-02-17 05:44:51 +0100

    add command init

--- src/presentation/cli/cli_main.py
+++ src/presentation/cli/cli_main.py
@@ -1,10 +1,13 @@
+import os
+
 import typer
+from dotenv import set_key, load_dotenv
 from rich.console import Console
 from sentry_sdk import set_user
 
 from helpers.helper_cli import error_display
 from helpers.helpers import get_current_user
-from src.infrastructures.database.session import get_session, init_db
+from src.infrastructures.database.session import get_session, init_db, init_postgresql
 from src.presentation.cli.commands.auth_commands import auth_app
 from src.presentation.cli.commands.client_commands import client_app
 from src.presentation.cli.commands.contrat_commands import contrat_app
@@ -36,11 +39,12 @@ def main(ctx: typer.Context):
 
 
     if ctx.obj["current_user"] is None:
-        if ctx.invoked_subcommand == "auth":
-            pass
-        if ctx.invoked_subcommand != "auth":
+
+        if ctx.invoked_subcommand not in ["auth", "init"]:
             error_display("Erreur Authentification", "Veuillez vous connecter via - auth login -")
             raise typer.Exit(1)
+        if ctx.invoked_subcommand in ["auth", "init"]:
+            pass
 
     if ctx.obj["current_user"] is not None:
         set_user({
@@ -52,9 +56,26 @@ def main(ctx: typer.Context):
 @app.command()
 def init():
     """Command initialize database"""
+    console.print("Veuillez vous connecter à votre Base de donnée via nos identifiant")
+    user = typer.prompt("Utilisateur", default="postgres")
+    password = typer.prompt("Mot de passe", default="")
+    db_name = typer.prompt("Nom de la base de donnée que vous allez créer")
+
     try:
-        console.print("[yellow]Initialisation de la base de données...[/yellow]")
+        result = init_postgresql(user, password, db_name)
+        if not result:
+            error_display("Existant", f"La base de donnée {db_name} existe déjà")
+            raise typer.Exit()
+        else:
+            console.print(f"[green]* Base de données {db_name} avec succès[/green]")
+
+        database_url = f"postgresql+psycopg2://{user}:{password}@localhost/{db_name}"
+        set_key(".env","DATABASE_URL",database_url)
+        os.environ["DATABASE_URL"] = database_url
+
+        console.print("[yellow]* Initialisation de la base de données...[/yellow]")
         init_db()
         console.print("[green]* Base de données initialisée avec succès![/green]")
+
     except Exception as e:
-        console.print(f"[red]✗ Erreur lors de l'initialisation: {str(e)}[/red]")
+        console.print(f"[red] Erreur lors de l'initialisation: {str(e)}[/red]")
